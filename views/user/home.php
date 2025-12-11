ab17995a794e18f2588486b5159a932531bf469a
ilhamjabbar-z1 <ilhamjabbar27@gmail.com>
2025-12-11 14:02:44 +0700

    updt

--- views/user/home.php
+++ views/user/home.php
@@ -1,7 +1,7 @@
 <?php include 'views/layouts/header.php'; ?>
 
 <div class="hero" style="position: relative; text-align: center;">
-    <img src="assets/images/hero-denim.jpg" style="width: 100%; height: 80vh; object-fit: cover;">
+    <img src="assets/img/img1.png" style="width: 100%; height: 80vh; object-fit: cover;">
     <h1 style="position: absolute; top: 40%; left: 10%; font-family: 'Teko'; font-size: 5rem; color: white; text-shadow: 2px 2px 4px rgba(0,0,0,0.5);">
         thanksjabbran®
     </h1>
